77148729d0cc729cf73f97a8aa3eb85d99a2eac0
Joseph Serrano <josephsteve08@gmail.com>
2021-05-24 10:32:26 -0700

    change "getStaticProps" and use "getServerSideProps"

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,18 +1,23 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getProducts } from '@/utils/Fauna';
 import ProductDataTable from '@/components/ProductDataTable';
 import Page from '@/components/layout/Page';
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const products = await getProducts();
-  return {
-    props: {
-      products,
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const products = await getProducts();
+    return {
+      props: { products }
     }
+  } catch (error) {
+    console.error(error);
+    context.res.statusCode = 302;
+    context.res.setHeader('Location', `/`);
+    return { props: {} };
   }
 }
 
-export default function Product({products}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Product({products}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <Page>
       <ProductDataTable products={products} />
--- pages/supplier/index.tsx
+++ pages/supplier/index.tsx
@@ -1,18 +1,23 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getSuppliers } from '@/utils/Fauna';
 import SupplierDataTable from '@/components/SupplierDataTable';
 import Page from '@/components/layout/Page';
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const suppliers = await getSuppliers();
-  return {
-    props: {
-      suppliers,
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const suppliers = await getSuppliers();
+    return {
+      props: { suppliers }
     }
+  } catch (error) {
+    console.error(error);
+    context.res.statusCode = 302;
+    context.res.setHeader('Location', `/`);
+    return { props: {} };
   }
 }
 
-export default function Supplier({suppliers}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Supplier({suppliers}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <Page>
       <SupplierDataTable suppliers={suppliers} />
